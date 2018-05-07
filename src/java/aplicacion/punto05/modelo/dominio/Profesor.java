8126a6a53814c638bdc7a4364b9a9509db53b4c7
Lopez2470 <>
2018-05-06 23:37:31 -0300

    Punto 05 - Lopez2470

---
+++ src/java/aplicacion/punto05/modelo/dominio/Profesor.java
@@ -0,0 +1,79 @@
+
+package aplicacion.punto05.modelo.dominio;
+
+
+public class Profesor {
+    private String legajo;
+    private String apellido;
+    private String nombre;
+    private String sexo;
+    
+
+    public Profesor() {
+    }
+
+    public Profesor(String legajo, String apellido, String nombre, String sexo) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the legajo
+     */
+    public String getLegajo() {
+        return legajo;
+    }
+
+    /**
+     * @param legajo the legajo to set
+     */
+    public void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+}
+
